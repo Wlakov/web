df54004be555fa2fc681f02220101aed5f230375
Wlakov <koval0310>
2021-10-11 12:11:05 +0300

    Done!

--- app.js
+++ app.js
@@ -9,6 +9,27 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname + '/index.html'));
 });
+app.get("/second", function(req, res){
+    res.sendFile(path.join(__dirname + '/second.html'));
+});
+app.get("/third", function(req, res){
+    res.sendFile(path.join(__dirname + '/third.html'));
+});
+app.get("/fourth", function(req, res){
+    res.sendFile(path.join(__dirname + '/fourth.html'));
+});
+app.get("/fifth", function(req, res){
+    res.sendFile(path.join(__dirname + '/fifth.html'));
+});
+app.get("/sixth", function(req, res){
+    res.sendFile(path.join(__dirname + '/sixth.html'));
+});
+app.get("/chess", function(req, res){
+    res.sendFile(path.join(__dirname + '/chess.html'));
+});
+app.get("/changeColor", function(req, res){
+    res.sendFile(path.join(__dirname + '/changeColor.html'));
+});
 
 
 app.listen(3000, function(){
